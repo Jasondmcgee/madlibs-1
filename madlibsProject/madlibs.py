2f790a227a6dbc990f39bfab178915ae54c716db
Kabsa <kabsaa@Guests-MacBook-Air.local>
2019-09-02 20:58:19 -0700

    add list

--- madlibsProject/madlibs.py
+++ madlibsProject/madlibs.py
@@ -1,3 +1,5 @@
 color = raw_input("Enter a color: ") # adjective
 costume = raw_input("Enter a noun: ") #noun
 verb = raw_input("Enter a verb ending in -ing: ") #verb
+
+partsOfSpeech = [color,costume,verb]
