ee9a818deebc1fcc08583f873d385030b6cbee50
Kabsa <kabsaa@Guests-MacBook-Air.local>
2019-09-02 21:05:05 -0700

    add print with story

--- madlibsProject/madlibs.py
+++ madlibsProject/madlibs.py
@@ -5,7 +5,8 @@ verb = raw_input("Enter a verb ending in -ing: ") #verb
 partsOfSpeech = [color,costume,verb]
 
 def fullMadlib():
-
-    # do stuff
+    print("Halloween is my favorite holiday because I get to dress up like a " + partsOfSpeech[1]+
+     ". Also, Fall is the best season because the leaves are changing from green to " + partsOfSpeech[0] +
+    ". My favorite part of Halloween is " + partsOfSpeech[2] + " lots of kitkats.")
 
 fullMadlib() #call function
